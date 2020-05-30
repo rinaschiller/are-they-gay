1d3660409cdc5d3ad03f774c48bf1b0275ebe6e7
Rina Schiller <schiller.rina@gmail.com>
2020-05-29 22:05:28 -0400

    better message for validation

--- persons/create.py
+++ persons/create.py
@@ -15,32 +15,30 @@ def create(event, context):
         logging.info(event)
         logging.error('Validation Failed')
         return {'statusCode': 422,
-                'body': json.dumps({'error_message': 'Couldn\'t create the person item.'})}
+                'body': json.dumps({'error_message': 'Couldn\'t create the person item because required fields not in JSON payload.'})}
 
    # TODO: handle if someone with that name already exists
 
-    try:
-        a_person = PersonModel(person_id=str(uuid.uuid1()),
-                               first_name=data.get('first_name'),
-                               last_name=data.get('last_name'),
-                               gender=data.get('gender'),
-                               is_gay=data.get('is_gay'),
-                               zodiac_sign=data.get('zodiac'),
-                               sexuality=data.get('sexuality'),
-                               pronouns=data.get('pronouns'),
-                               image_url=data.get('image_url'),
-                               twitter_handle=data.get('twitter_handle'))
+    a_person = PersonModel(person_id=str(uuid.uuid1()),
+                           first_name=data.get('first_name'),
+                           last_name=data.get('last_name'),
+                           gender=data.get('gender'),
+                           is_gay=data.get('is_gay'),
+                           zodiac_sign=data.get('zodiac'),
+                           sexuality=data.get('sexuality'),
+                           pronouns=data.get('pronouns'),
+                           image_url=data.get('image_url'),
+                           twitter_handle=data.get('twitter_handle'))
 
-        # write the person to the database
-        a_person.save()
+    # write the person to the database
+    a_person.save()
 
-        # create a response
-        return {'statusCode': 201,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': 'true',
-                },
-                'body': json.dumps(dict(a_person))}
-    except Exception as e:
-        return {'statusCode': 400,
-                'body': json.dumps({'error_message': 'failed for exception {}'.format(e)})}
+    logging.info('Person {} {} successfully created'.format(a_person.first_name, a_person.last_name))
+    # create a response
+    return {'statusCode': 201,
+            'message': 'Person successfully created',
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': 'true',
+            },
+            'body': json.dumps(dict(a_person))}
